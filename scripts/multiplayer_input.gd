065e37239ab0fff2e2c5d5d21e372662d6652e49
ricky <ricky.y.c@outlook.com>
2025-06-29 02:35:16 +0800

    correct way to use dict

--- scripts/multiplayer_input.gd
+++ scripts/multiplayer_input.gd
@@ -1,51 +1,52 @@
 class_name PlayerInput
 extends Node
 
-var move_l := Vector4(0, -1e10, -1e10, -1e10)
-var move_r := Vector4(0, -1e10, -1e10, -1e10)
-var jump := Vector4(0, -1e10, -1e10, -1e10)
+const _EMPTY_ACTION_DICT := {
+	"strength": 0,
+	"pressed_tick": -INF, # FIXME: maybe use int
+	"just_pressed_tick": -INF, 
+	"just_released_tick": -INF,
+}
 
-var _move_l_buffer := move_l
-var _move_r_buffer := move_r
-var _jump_buffer := jump
+@export var action_names: PackedStringArray
+var actions : Dictionary
+var _backup : Dictionary
+
+func _get(property: StringName) -> Variant:
+	if property not in action_names: return null
+	return actions[property]
 
 func _ready() -> void:
 	NetworkTime.before_tick_loop.connect(_gather)
 	NetworkTime.after_tick.connect(_reset)
+	
+	for action in action_names:
+		_backup[action] = _EMPTY_ACTION_DICT.duplicate(true)
 
 var _samples := 0
 func _process(delta: float) -> void:
 	_samples += 1
 	
-	_jump_buffer = _make_buffer(_jump_buffer, "jump")
-	_move_l_buffer = _make_buffer(_move_l_buffer, "move_l")
-	_move_r_buffer = _make_buffer(_move_r_buffer, "move_r")
+	for action in action_names:
+		_backup[action].strength += Input.get_action_strength(action)
+		if Input.is_action_pressed(action):
+			_backup[action].pressed_tick = NetworkTime.tick
+		if Input.is_action_just_pressed(action):
+			_backup[action].just_pressed_tick = NetworkTime.tick
+		if Input.is_action_just_released(action):
+			_backup[action].just_released_tick = NetworkTime.tick
 
 func _gather() -> void:
 	if not is_multiplayer_authority():
 		return
 	
-	jump = _gather_from(_jump_buffer)
-	move_l = _gather_from(_move_l_buffer)
-	move_r = _gather_from(_move_r_buffer)
+	actions = _backup.duplicate(true) # deep clone
+	for action in action_names:
+		if _samples == 0: continue
+		actions[action].strength /= _samples
 
 func _reset(delta: float, tick: int) -> void:
 	_samples = 0
 	
-	_jump_buffer.x = 0
-	_move_l_buffer.x = 0
-	_move_r_buffer.x = 0
-
-func _make_buffer(_buffer: Vector4, action: String) -> Vector4:
-	_buffer.x += Input.get_action_strength(action)
-	if Input.is_action_pressed(action):
-		_buffer.y = NetworkTime.tick
-	if Input.is_action_just_pressed(action):
-		_buffer.z = NetworkTime.tick
-	if Input.is_action_just_released(action):
-		_buffer.w = NetworkTime.tick
-	return _buffer
-func _gather_from(_buffer: Vector4) -> Vector4:
-	if _samples > 0:
-		_buffer.x /= _samples
-	return _buffer
+	for action in action_names:
+		_backup[action].strength = 0.
